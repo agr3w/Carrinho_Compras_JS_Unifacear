5313fb24bc5cf6e2b2516742a05f69ae5bcac9a0
agr3w <suzanakampa.sk@gmail.com>
2025-10-06 09:41:21 -0300

    feat: adicionando comentários

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 let total = 0;
-let contadorItens = 0; // Nova variável para contar os itens no carrinho
+let contadorItens = 0;
 
 //função para adicionar item no carrinho
 function adicionarCarrinho(titulo, preco) {
+    // Cria um novo item na lista do carrinho, onde todos os elementos ficaram ancorados
     const item = document.createElement("li");
     item.innerText = `${titulo} - R$ ${preco.toFixed(2)}`;
 
-    //crio o botão que remove o item específico do nosso carrinho
+    //crio o botão que remove o item específico do carrinho
     const botaoRemover = document.createElement("button");
-    botaoRemover.innerText = "Remover";
-    botaoRemover.style.marginLeft = "10px";
-    botaoRemover.onclick = function () {
+    botaoRemover.innerText = "Remover"; // Texto do botão
+    botaoRemover.style.marginLeft = "10px"; // Estilo do botão
+    botaoRemover.onclick = function () { // Função que remove o item do carrinho
         item.remove();
-        total -= preco;
-        contadorItens--;
+        total -= preco; // Atualiza o total
+        contadorItens--; // Atualiza o contador de itens
         atualizarCarrinho();
     };
 
-    item.appendChild(botaoRemover);
-    document.getElementById("carrinho").appendChild(item);
+    item.appendChild(botaoRemover); // Adiciona o botão ao item do carrinho
+    document.getElementById("carrinho").appendChild(item); // Adiciona o item à lista do carrinho
 
     total += preco;
     contadorItens++;
@@ -31,6 +32,7 @@ function limparCarrinho() {
     // Pedir confirmação antes de limpar
     const confirmacao = confirm("Tem certeza de que deseja esvaziar o carrinho?");
 
+    // Se o usuário confirmar, esvazia o carrinho
     if (confirmacao) {
         document.getElementById("carrinho").innerHTML = "";
         total = 0;
